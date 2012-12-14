3867fa1bba866622a7b854ef9e706a4b9d6dbaf4
JStyleConcept <jstyleconcept@gmail.com>
2012-12-14 12:17:13 +0800

    2012/12/14 First Commit

---
+++ src/net/jstyleconcept/android/alertdialog/SourceCodeActivity.java
@@ -0,0 +1,18 @@
+package net.jstyleconcept.android.alertdialog;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.webkit.WebView;
+
+public class SourceCodeActivity extends Activity {
+	WebView sourceCodeView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_source_code);
+		sourceCodeView=(WebView)findViewById(R.id.webView1);
+		sourceCodeView.getSettings().setBuiltInZoomControls(true);
+		sourceCodeView.loadUrl("file:///android_asset/sourcecode.html");
+	}
+}
